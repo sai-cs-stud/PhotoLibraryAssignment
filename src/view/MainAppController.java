d404e11cf61e8f0d6ba1851bf9d306c89ab14881
NickyGund <neg62@scarletmail.rutgers.edu>
2020-11-15 23:11:38 -0500

    highlighter but it sucks

--- src/view/MainAppController.java
+++ src/view/MainAppController.java
@@ -5,21 +5,28 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.TilePane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 
 
 @SuppressWarnings("unused")
@@ -61,18 +68,43 @@ public class MainAppController {
 			File newphoto = fil_chooser.showOpenDialog(mainStage);
 			
 			if(newphoto != null) {
-				
+				try {
 				System.out.println(newphoto.getAbsolutePath());
 				String myphotopath = newphoto.getAbsolutePath();
-				Image myphoto = new Image(new FileInputStream(myphotopath),150, 0, true, true);
-				
+				File photofile = new File(myphotopath);
+				Image myphoto = new Image(new FileInputStream(photofile),150, 0, true, true);
+				StackPane background = new StackPane();
 				ImageView newimage = new ImageView(myphoto);
 				newimage.setFitWidth(150);
-				
+				newimage.setFitHeight(mainStage.getHeight() - 10);
+                newimage.setPreserveRatio(true);
+                newimage.setSmooth(true);
+                newimage.setCache(true);
+                
+				mytilepane.setPadding(new Insets(15, 15, 15, 15));
+		        mytilepane.setHgap(15);
 				mytilepane.getChildren().addAll(newimage);
-				mytilepane.setHgap(10);
-				mytilepane.setVgap(10);
-		       
+				
+				
+				newimage.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+					@Override
+					public void handle(MouseEvent mouseEvent) {
+						// TODO Auto-generated method stub
+						if(mouseEvent.getButton().equals(MouseButton.PRIMARY)){
+							
+							newimage.setEffect(new DropShadow(15, Color.BLACK));
+							//maybe need to check if other images are highlighted so that only one is highlighted at a time
+						
+						}
+					
+					}
+					
+				});
+				}
+				finally {
+					System.out.println("error");
+				}
 			}
 			
 		}
