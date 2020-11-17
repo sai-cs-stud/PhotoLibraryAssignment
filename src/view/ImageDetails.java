127d83d2204e7eb7bc94ed77b5572a4ae62ba798
sai-cs-stud <srm275@scarletmail.rutgers.edu>
2020-11-17 14:51:42 -0500

    added image path as field

--- src/view/ImageDetails.java
+++ src/view/ImageDetails.java
@@ -3,14 +3,18 @@ package view;
 import java.util.Calendar;
 
 public class ImageDetails {
-
+	String image_path;
 	Calendar date_time;
 	String Caption;
 	String[] tags;
-	public ImageDetails(Calendar date_time, String caption, String[] tags) {
+	public ImageDetails(String image_path, Calendar date_time, String caption, String[] tags) {
+		this.image_path = image_path;
 		this.date_time = date_time;
 		this.Caption = caption;
 		this.tags = tags;
+	}
+	public void resetImagePath(String image_path) {
+		this.image_path = image_path;
 	}
 	public void resetDate_Time(Calendar date_time) {
 		this.date_time = date_time;
