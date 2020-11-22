40258e3c080c563cb6cdfe75594f9d65c3b393fd
NickyGund <neg62@scarletmail.rutgers.edu>
2020-11-22 18:06:32 -0500

    doc fix

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"app"},{"l":"view"}]
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"app"}]
