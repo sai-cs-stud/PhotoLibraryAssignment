5825eaeac802e56a272fc6db503bbe995f03319a
sai-cs-stud <srm275@scarletmail.rutgers.edu>
2020-11-22 18:24:29 -0500

    multi-user

--- src/view/PhotoController.java
+++ src/view/PhotoController.java
@@ -48,8 +48,8 @@ public class PhotoController {
 						if(logininput.equals("stock")) {
 							isStock = true;
 						}
-						//File file = new File(Photos.storeDir + File.separator + logininput + ".bin");
-						//file.createNewFile();
+						File file = new File(Photos.storeDir + File.separator + logininput + ".bin");
+						file.createNewFile();
 						mainAppStage();
 						break;
 						
